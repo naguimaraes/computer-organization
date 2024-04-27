72707ce650ec391636526b3d7f9b4618d1c18be0
neitaans <naguimaraes@inf.ufrgs.br>
2024-04-27 13:34:36 -0300

    SRA adicionada no multiciclo, mas falta testar!!

---
+++ Trabalho 1/testbench-multiciclo.asm
@@ -0,0 +1,22 @@
+# Como a memoria do multiciclo eh compartilhada, convencionamos que
+# as instrucoes comecam no endereco 0 e os dados comecam
+# no endereco 0x000000a0. O registrador $10 serve de ponteiro para os dados.
+
+.text
+	LW $10, 0x000000a0
+.org 0
+inicio:
+	LW $1, 0($10)
+	LW $2, 4($10)
+	LW $3, 8($10)
+	SRA $3, $3, 4
+	SW $3, 8($10)
+	ADD $3, $1, $2
+	SUB $3, $3, $2
+	BEQ $3, $1 LB1
+	SW $0, 8($10)
+	J inicio
+LB1:
+	SW $3, 12($10)
+	J inicio
+
